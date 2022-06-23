46152b7b554d88b15a5cd649b2679667aa8e0a0e
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-23 10:21:25 -0400

    created tables for department, role, employee in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,53 @@
+-- Drop the database employee_db if it already exists --
+DROP DATABASE IF EXISTS employee_db;
+
+-- Create the database employee_db --
+CREATE DATABASE employee_db;
+
+-- Use employee_db for creating tables -- 
+USE employee_db;
+
+-- Create the table for department -- 
+CREATE TABLE department (
+
+    -- Column for department id --
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+    -- Column for department name --
+    name VARCHAR(30) NOT NULL
+);
+
+-- Create the table for role --
+CREATE TABLE role (
+
+    -- Column for role id --
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    
+    -- Column for role title 
+    title VARCHAR(30) NOT NULL, 
+
+    -- Column for role salary --
+    salary VARCHAR(30) NOT NULL, 
+
+    -- Column for role department name as id 
+    department_id INT NOT NULL
+);
+
+-- Create the table for employee --
+CREATE TABLE employee (
+
+    -- Column for employee id -- 
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+    -- Column for employee first name -- 
+    first_name VARCHAR(30) NOT NULL,
+
+    -- Column for employee last name -- 
+    last_name VARCHAR(30) NOT NULL, 
+
+    -- Column for employee role id -- 
+    role_id INT NOT NULL, 
+
+    -- Column for employee manager id -- 
+    manager_id INT
+);
